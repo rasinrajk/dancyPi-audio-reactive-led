bf0614c6602dd1e03653aec7b7c834b1e5922d62
deeputg <deeputg1992@gmail.com>
2020-02-18 23:52:13 +0530

    test updates

--- python/customVisualization.py
+++ python/customVisualization.py
@@ -196,7 +196,9 @@ def visualize_rainbow(y):
     r=0
     # g = int(np.mean(y[len(y) // 3: 2 * len(y) // 3]**scale))
     g=0
-    b = int(np.mean(y[2 * len(y) // 3:]**scale))
+    # b = int(np.mean(y[2 * len(y) // 3:]**scale))
+    b=0
+    
     # Assign color to different frequency regions
     p[0, :r] = 255.0
     p[0, r:] = 0.0
