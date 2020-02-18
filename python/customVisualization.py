8f9975e24b5f54993dfef706e62dc0dd64e8838d
deeputg <deeputg1992@gmail.com>
2020-02-18 23:34:00 +0530

    test updates

--- python/customVisualization.py
+++ python/customVisualization.py
@@ -188,7 +188,7 @@ def visualize_rainbow(y):
     y /= gain.value
     # Scale by the width of the LED strip
     # y *= float((config.N_PIXELS // 2) - 1)
-    y *= float((config.N_PIXELS//3) - 1)
+    y *= float((config.N_PIXELS//4) - 1)
     # Map color channels according to energy in the different freq bands
     scale = 0.9
     r = int(np.mean(y[:len(y) // 3]**scale))
