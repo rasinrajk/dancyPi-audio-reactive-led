5c776d55882c41ba0d62c41b698c934725cd2f5b
deeputg <deeputg1992@gmail.com>
2020-02-19 02:24:09 +0530

    test updates

--- python/customVisualization.py
+++ python/customVisualization.py
@@ -267,11 +267,11 @@ def visualize_rainbow2(y):
     p[1, 0] = r
     p[2, 0] = r
     p[0, 1] = b
-    p[1, 2] = b
-    p[2, 3] = b
-    p[0, 4] = g
-    p[1, 5] = g
-    p[2, 6] = g
+    p[1, 1] = b
+    p[2, 1] = b
+    p[0, 2] = g
+    p[1, 2] = g
+    p[2, 2] = g
 
     # Update the LED strip
     return np.concatenate((p[:, ::-1], p), axis=1)
