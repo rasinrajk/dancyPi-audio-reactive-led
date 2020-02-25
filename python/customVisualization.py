aea7ddec0e047a83c5c132f48e6546341937dd4e
deeputg <deeputg1992@gmail.com>
2020-02-25 09:04:48 +0530

    test changes

--- python/customVisualization.py
+++ python/customVisualization.py
@@ -276,8 +276,8 @@ def visualize_rainbow2(y):
 
     #test 2
     p[0, 0] = r
-    p[1, 0] = r
-    p[2, 0] = r
+    p[1, 0] = g
+    p[2, 0] = b
     # p[0, 1] = b
     # p[1, 1] = b
     # p[2, 1] = b
